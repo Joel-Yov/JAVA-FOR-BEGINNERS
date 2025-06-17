c2025dd22eeac2dbe603d48750a8079b1165d838
Joel Yovera <ingyovera6114@gmail.com>
2025-06-17 12:36:14 -0500

    Tu primer hola mundo

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        //THIS IS MY FIRST JAVA'S EXERCISE
+        /*
+            H
+            E
+            L
+            L
+            O
+         */
+        System.out.print("JAVA FOR BEGINNERS");
+    }
+}

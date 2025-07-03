2f9ee857c6410391d5781684bc23b77d0251c123
Joel Yovera <ingyovera6114@gmail.com>
2025-07-02 23:20:34 -0500

    Using constants in JAVA

---
+++ src/Constants.java
@@ -0,0 +1,14 @@
+public class Constants {
+    public static void main(String[] args) {
+        /*Constants in Java
+         The main difference between variable and constant is: The constant never changes its value
+
+         USE final for declaration constants for example:
+
+         USE CAPITAL LETTERS FOR DECLARATION CONSTANT AND HYPHENS: MY_CONSTANT
+         */
+
+        final int MY_CONSTANT = 20;
+        System.out.println("My constant is: " + MY_CONSTANT);
+    }
+}

2879eb22c1964a214bfc2c1dafb9b28b5f61ab24
Joel Yovera <ingyovera6114@gmail.com>
2025-07-05 00:18:17 -0500

    New topic about relational operators: ==, !=, <, <=, >, >=

---
+++ src/RelationalOperators.java
@@ -0,0 +1,28 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        //==
+        int a=3, b=3;
+        var resultado = a == b;
+        System.out.println("RESULTADO: " + resultado);
+
+        //Distinto !=
+        resultado = a != b;
+        System.out.println("RESULTADO: " + resultado);
+
+        // >
+        resultado = a > b;
+        System.out.println("RESULTADO: " + resultado);
+
+        // >=
+        resultado = a >= b;
+        System.out.println("RESULTADO: " + resultado);
+
+        // <
+        resultado = a < b;
+        System.out.println("RESULTADO: " + resultado);
+
+        // <=
+        resultado = a <= b;
+        System.out.println("RESULTADO: " + resultado);
+    }
+}

fa64f4072812493db51f657624880276717bd4cd
Joel Yovera <ingyovera6114@gmail.com>
2025-07-04 23:59:15 -0500

    First EXERCISE

--- src/OperatorUnarios.java
+++ src/OperatorUnarios.java
@@ -1,5 +1,18 @@
 public class OperatorUnarios {
     public static void main(String[] args) {
+        //DECLARATION VARIABLES
+        int a = 3, b = -2, resultado = 0;
+        boolean c = false;
+
+        //OPERATOR UNARIO -
+        resultado = -a;
+        System.out.println("RESULTADO: " + resultado);
+
+        //OPERATOR UNARIO -
+        resultado = -b;
+        System.out.println("RESULTADO: " + resultado);
+
+
 
     }
 }

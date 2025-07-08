0b06ade0d3617fe1d74a8a67b5bdb199bf9dc40b
Joel Yovera <ingyovera6114@gmail.com>
2025-07-08 13:40:02 -0500

    New Topic: Asignación de variables =, +=, -=, *=

--- src/OPERATORS/AsignacionOperator.java
+++ src/OPERATORS/AsignacionOperator.java
@@ -1,4 +1,25 @@
 package OPERATORS;
 
 public class AsignacionOperator {
+    public static void main(String[] args) {
+
+        //ASIGNACIÓN
+        var myNumber = 10;
+        int mySecondNumber; //Only declaration, no asignación
+        mySecondNumber = 5;
+
+        //ASIGNACIÓN COMPUESTA
+
+        //+=
+        mySecondNumber += 5;
+        System.out.println(mySecondNumber);
+
+        //-=
+        mySecondNumber -= 5;
+        System.out.println(mySecondNumber);
+
+        //*=
+        mySecondNumber *= 5;
+        System.out.println(mySecondNumber);
+    }
 }

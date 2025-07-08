ecc4df34274e655a7cafe28a01383a22aa7ac001
Joel Yovera <ingyovera6114@gmail.com>
2025-07-08 13:40:48 -0500

    Ejercicios de repaso

--- src/OPERATORS/AreaRectangulo.java
+++ src/OPERATORS/AreaRectangulo.java
@@ -1,4 +1,22 @@
 package OPERATORS;
 
+import java.util.Scanner;
+
 public class AreaRectangulo {
+    public static void main(String[] args) {
+        Scanner consola = new Scanner(System.in);
+
+        System.out.print("Ingresa el valor de la base: ");
+        int base = Integer.parseInt(consola.nextLine());
+
+        System.out.print("Ingresa el valor de la altura: ");
+        int altura = Integer.parseInt(consola.nextLine());
+
+        int perimetro = (base+altura)*2;
+
+        int area = base * altura;
+
+        System.out.println("El área es: " + area + " m^2");
+        System.out.println("El perímetro es: " + perimetro + " m");
+    }
 }
--- src/OPERATORS/ejercicioDentroRango.java
+++ src/OPERATORS/ejercicioDentroRango.java
@@ -1,4 +1,16 @@
 package OPERATORS;
 
+import java.util.Scanner;
+
 public class ejercicioDentroRango {
+    public static void main(String[] args) {
+        Scanner consola = new Scanner(System.in);
+
+        System.out.print("Write your number: ");
+        int number = Integer.parseInt(consola.nextLine());
+
+        boolean resultado = number >= 0 && number <=5;
+
+        System.out.println(resultado);
+    }
 }
